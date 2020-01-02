7f7dde47e6eddaf5a967d65e1844cbc250dfd111
atefgad <atef0777@gmail.com>
2020-01-02 09:53:57 +0200

    lesson 18

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,53 @@ $(function(){
 
     // sidebar submenu toggle
     $(".toggle-submenu").on("click", function(){
-        $(this).toggleClass("fa-angle-right fa-angle-down");
-        $(this).parent('a').next(".child-links").slideToggle();
+        $(this).find(".fa-angle-right").toggleClass("down");
+        $(this).next(".child-links").slideToggle();
     })
+
+    // hamburger-icon
+    $(".hamburger-icon").on("click",function() {
+        $(this).toggleClass("open");
+    })
+
+    // fullScreen
+    $(".fullScreen").on("click", function() {
+        $(this).toggleClass("openFullScreen");
+        if($(this).hasClass("openFullScreen")){
+            openFullscreen()
+        }else{
+            closeFullscreen()
+        }
+    })
+
 })
+
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
